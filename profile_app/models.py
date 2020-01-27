21c41be15722470ee7591a0d8c96a420d39a4655
akshay.jain <jainakshay.aj98@gmail.com>
2020-01-27 11:50:47 +0530

    customized User Profile so user can login through their email.

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, name, email, password=None):
+        email = self.normalize_email(email)
+        user = self.model(name=name, email=email)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, name, email, password):
+        user = self.create_user(name, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
